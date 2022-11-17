9fc463bc33ce97d41fcf58ef5b1716df38fd290b
shady bassily <shadybassily@yahoo.com>
2022-11-17 16:14:32 +0200

    animating download cv button

--- src/components/about/DownloadCV.component.jsx
+++ src/components/about/DownloadCV.component.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import cv from "../../assets/cv/Shady_Bassily_cv_front_end.pdf";
-import { BsFillCloudDownloadFill } from "react-icons/bs";
-import { AiOutlineCheck } from "react-icons/ai";
-
+import { BsFillCloudDownloadFill } from "react-icons/bs"; //download
+import { AiOutlineCheck } from "react-icons/ai"; //checked
 import { motion } from "framer-motion";
+
 export default function DownloadCV() {
   const [isDownload, setIsDownload] = useState(false);
-  const toggleSubmit = () => {
+  const toggleDownload = () => {
     setIsDownload(true);
   };
 
@@ -14,52 +14,33 @@ export default function DownloadCV() {
     if (isDownload) {
       setTimeout(() => {
         setIsDownload(false);
-      }, 1500);
+      }, 1000);
     }
   }, [isDownload]);
 
-  const downloadButton = {
-    clicked: { borderRadius: "45%", width: 40 },
-    notClicked: {},
+  const downloadText = {
+    clicked: { y: [0, -40, -40, 0] },
   };
 
-  const downloadText = {
-    clicked: { y: [0, -50, -50, -50, 0], x: [0, 0, 0, 0, 30] },
-    notClicked: {},
-  };
-  const checked = {
-    clicked: { y: [0, -33, -33, -33, 0] },
-    notClicked: {},
-  };
   return (
     <div className="download-btn-container">
-      <motion.button
-        className="download"
-        onClick={toggleSubmit}
-        variants={downloadButton}
-        animate={isDownload ? "clicked" : "notClicked"}
-        transition={{ duration: 0.3 }}
-      >
-        <motion.a
-          href={cv}
-          download
+      <a className="download-btn" onClick={toggleDownload} href={cv} download>
+        <motion.div
           variants={downloadText}
-          transition={{ duration: 1 }}
-          className="download-text"
+          className="dowload-text-container"
+          animate={isDownload ? "clicked" : "notClicked"}
         >
-          download CV{" "}
-          <span className="icon">
-            <BsFillCloudDownloadFill />
-          </span>
-        </motion.a>
-        <motion.a
-          className="checked"
-          variants={checked}
-          transition={{ duration: 1.4 }}
-        >
-          <AiOutlineCheck />
-        </motion.a>
-      </motion.button>
+          <p className="download-text">
+            download CV{" "}
+            <span className="download-icon">
+              <BsFillCloudDownloadFill />
+            </span>
+          </p>
+          <p className="download-complete">
+            <AiOutlineCheck />
+          </p>
+        </motion.div>
+      </a>
     </div>
   );
 }
