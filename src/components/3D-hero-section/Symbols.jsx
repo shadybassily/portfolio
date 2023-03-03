82651f8126a688c5214ee31b3d5bae769976fa80
shady bassily <shadybassily@yahoo.com>
2023-03-03 22:11:46 +0200

    test

--- src/components/3D-hero-section/Symbols.jsx
+++ src/components/3D-hero-section/Symbols.jsx
@@ -1,7 +1,7 @@
 import { Float, Text3D, useGLTF } from '@react-three/drei';
 const Symbols = ({commonTextOptions,width}) => {
-    const { scene: reactIcon } = useGLTF("../../../public/models/reactSymbol.glb");
-    reactIcon.scale.set(4, 4, 3.5);
+    // const { scene: reactIcon } = useGLTF("../../../public/models/reactSymbol.glb");
+    // reactIcon.scale.set(4, 4, 3.5);
  
     const tagPoisiton = width < 800 ? [1, 1.2, 0.5] : [3, 1, 0.3];
     const reactIconPosition = width < 800 ? [-1.7, -0.5, -0.5] : [0.5, -1.3, -0.3];
@@ -12,7 +12,7 @@ const Symbols = ({commonTextOptions,width}) => {
                 {'</>'}
                 <meshStandardMaterial color="#3498db" />
              </Text3D>
-             <primitive object={reactIcon} position={reactIconPosition} rotation={[-0.3, 1.3, 0.3]} />
+             {/* <primitive object={reactIcon} position={reactIconPosition} rotation={[-0.3, 1.3, 0.3]} /> */}
        </Float>
     )
  }
