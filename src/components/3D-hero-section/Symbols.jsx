87a9154fcb8b95ce0b471bf23232d63c27a76b7c
shady bassily <shadybassily@yahoo.com>
2023-03-04 10:51:30 +0200

    attempt to fix model bug

--- src/components/3D-hero-section/Symbols.jsx
+++ src/components/3D-hero-section/Symbols.jsx
@@ -1,8 +1,16 @@
 import { Float, Text3D, useGLTF } from '@react-three/drei';
+
+
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+
+const symbolPath = "./public/models/cube1.glb"
 const Symbols = ({commonTextOptions,width}) => {
-    // const { scene: reactIcon } = useGLTF("../../../public/models/reactSymbol.glb");
-    // reactIcon.scale.set(4, 4, 3.5);
- 
+   //  const { scene: reactIcon } = useGLTF("../../assets/models/cube1.glb");
+   //  reactIcon.scale.set(1, 1, 1);
+   
+   const gltf = useLoader(GLTFLoader, symbolPath)
     const tagPoisiton = width < 800 ? [1, 1.2, 0.5] : [3, 1, 0.3];
     const reactIconPosition = width < 800 ? [-1.7, -0.5, -0.5] : [0.5, -1.3, -0.3];
  
@@ -12,7 +20,7 @@ const Symbols = ({commonTextOptions,width}) => {
                 {'</>'}
                 <meshStandardMaterial color="#3498db" />
              </Text3D>
-             {/* <primitive object={reactIcon} position={reactIconPosition} rotation={[-0.3, 1.3, 0.3]} /> */}
+             <primitive object={gltf.scene} position={reactIconPosition} rotation={[-0.3, 1.3, 0.3]} />
        </Float>
     )
  }
