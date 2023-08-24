b915b1ea692c13030f8595b4bace8f2be76e696f
Shady <shadybassily@yahoo.com>
2023-08-24 19:37:59 +0300

    logo changed

--- src/components/navbar/Navbar.component.jsx
+++ src/components/navbar/Navbar.component.jsx
@@ -1,34 +1,35 @@
 import React, { useEffect, useState } from "react";
 import "./navbar.styles.css";
 import { motion, useScroll } from "framer-motion";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo.svg";
 import ProgressBar from "../progress-bar/ProgressBar.component";
 import NavMenu from "./navmenu/NavMenu.component";
 export default function Navbar() {
-  const { scrollY } = useScroll();
-  const [currentScroll, setCurrentScroll] = useState(0);
-  useEffect(() => {
-    return scrollY.onChange((latest) => {
-      setCurrentScroll(latest);
-    });
-  }, []);
+	const { scrollY } = useScroll();
+	const [currentScroll, setCurrentScroll] = useState(0);
+	useEffect(() => {
+		return scrollY.onChange((latest) => {
+			setCurrentScroll(latest);
+		});
+	}, []);
 
-  return (
-    <motion.nav
-      animate={{
-        paddingTop: currentScroll > 50 ? 0 : 5,
-        paddingBottom: currentScroll > 50 ? 0 : 5,
-      }}
-      transition={{ duration: 0.3 }}>
-      <motion.div
-        className="logo-container"
-        animate={{ rotate: currentScroll > 50 ? 90 : 0 }}
-        transition={{ duration: 0.3 }}>
-        <img className="logo" src={logo} alt="logo" />
-      </motion.div>
+	return (
+		<motion.nav
+			animate={{
+				paddingTop: currentScroll > 50 ? 0 : 5,
+				paddingBottom: currentScroll > 50 ? 0 : 5,
+			}}
+			transition={{ duration: 0.3 }}
+		>
+			<motion.div
+				animate={{ rotate: currentScroll > 50 ? 90 : 0 }}
+				transition={{ duration: 0.3 }}
+			>
+				<img className="logo" src={logo} alt="logo" />
+			</motion.div>
 
-      <NavMenu />
-      <ProgressBar />
-    </motion.nav>
-  );
+			<NavMenu />
+			<ProgressBar />
+		</motion.nav>
+	);
 }
